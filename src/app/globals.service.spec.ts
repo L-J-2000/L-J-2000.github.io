264d1a062af70873f0f078240aa1c21cd6bc3c1d
L-J-2000 <139916573+L-J-2000@users.noreply.github.com>
2024-08-09 06:12:50 +0200

    add karma config

--- src/app/globals.service.spec.ts
+++ src/app/globals.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, provideRouter } from '@angular/router';
 import { GlobalsService } from './globals.service';
+import { routes } from '../routes';
 
 describe('GlobalsService', () => {
   let service: GlobalsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [RouterTestingModule.withRoutes(routes)],
+      providers: [Router],
+    });
     service = TestBed.inject(GlobalsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return "true" for "routeShouldWidenMasthead" with /portfolio'),
+    () => {};
 });
