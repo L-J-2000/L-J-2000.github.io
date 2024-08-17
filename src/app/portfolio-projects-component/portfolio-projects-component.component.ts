1abc968f2b453191d8562831c464c42c6cd49b35
L-J-2000 <139916573+L-J-2000@users.noreply.github.com>
2024-08-17 14:59:52 +0200

    display projectDetailsList with test values

--- src/app/portfolio-projects-component/portfolio-projects-component.component.ts
+++ src/app/portfolio-projects-component/portfolio-projects-component.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectDetails } from 'src/global-interfaces';
 
 @Component({
   selector: 'app-portfolio-projects-component',
   templateUrl: './portfolio-projects-component.component.html',
-  styleUrls: ['./portfolio-projects-component.component.css']
+  styleUrls: ['./portfolio-projects-component.component.css'],
 })
 export class PortfolioProjectsComponentComponent {
-
+  @Input({ required: true }) projectDetails!: ProjectDetails[];
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,16 +7,19 @@ import { ProjectDetails } from '../../global-interfaces';
   styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent {
-  projectList: ProjectDetails[] = [
-    {
-      id: 0,
-      title: 'hello world',
-      description: 'This is my first project',
-      imageURL: '../../assets/img/AngularPlaceHolder.jpg',
-    },
-  ];
-
-  indexOfNextProject = this.projectList.length;
+  projectList: ProjectDetails[];
+  indexOfNextProject = 0;
+  constructor() {
+    this.projectList = [
+      {
+        id: 0,
+        title: 'hello world',
+        description: 'This is my first project',
+        imageURL: '../../assets/img/AngularPlaceHolder.jpg',
+      },
+    ];
+    this.indexOfNextProject = this.projectList.length;
+  }
 
   addProjectToList(projectDetailEntry: ProjectDetails) {
     this.projectList[this.indexOfNextProject] = projectDetailEntry;
